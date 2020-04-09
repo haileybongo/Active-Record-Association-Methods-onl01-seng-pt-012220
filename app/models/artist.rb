0551b1c1b799a32822ea0b5a9a2f70feb7ed45b9
Hailey Bongo <haileyebongo@gmail.com>
2020-04-09 18:55:40 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,7 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
-   Self.all.find_by(artist: self, )
+   Self.songs
   end
 
   def song_count
