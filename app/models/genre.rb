1b7eb175440c7fdf5267cbb46baafa3df76e805c
Hailey Bongo <haileyebongo@gmail.com>
2020-04-09 19:00:07 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,10 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.name 
+    names = []
+    self.artists.each do |artist|
+      names << artist.name
+    end
+    names
   end
 end
